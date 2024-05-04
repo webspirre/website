44b1c476f881b4625c4e3e49391da61d5be083c5
StephenChi-hi <nwikechisom77@gmail.com>
2024-05-04 17:41:58 +0100

    hi

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -7,12 +7,10 @@ async function Page() {
    
   return (
     <div className=" items-center grid grid-cols-2 gap-4">
-      {/* Content for the first column */}
       <div className="bg-white px-4">
         <Form />
       </div>
 
-      {/* Content for the second column */}
       <div className="bg-white px-4">
         <Animation />
       </div>
