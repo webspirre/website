65c91a85304b32dba711aed122fb89ef6d04bae5
StephenChi-hi <nwikechisom77@gmail.com>
2024-06-07 21:19:08 +0100

    bug fix

--- src/componet/browse_designs/Tabs.js
+++ src/componet/browse_designs/Tabs.js
@@ -75,7 +75,7 @@ const HorizontalTabs = ({ tabs, data }) => {
         <button
           key={index}
           onClick={() => setDeviceFilter(device)}
-          className={`pr-4 py-2 mb-4 border-b-2 text-[14px] ${
+          className={`mr-4 px-2 py-2 mb-4 border-b-2 text-[14px] ${
             deviceFilter === device
               ? "border-gray-500 text-gray-700"
               : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-500"
