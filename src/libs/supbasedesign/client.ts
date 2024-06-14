62c5cf95013943f634fb5fa8b8cc67a7f661b6ea
webspirrepro <webspirre@gmail.com>
2024-06-14 17:10:32 +0100

    resolved

--- src/libs/supbasedesign/client.ts
+++ src/libs/supbasedesign/client.ts
@@ -5,6 +5,6 @@ import { Database } from "@/types/types_db";
 export const createClient = () =>
   createBrowserClient<Database>(
     // Pass Supabase URL and anonymous key from the environment to the client
-    process.env.NEXT_PUBLIC_SUPABASE_URL_DESIGN! || "https://jplgamffqvnzruoyxdyg.supabase.co",
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY_DESIGN! || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwbGdhbWZmcXZuenJ1b3l4ZHlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5MTQ4NTYsImV4cCI6MjAzMjQ5MDg1Nn0.DGkv3zLoxjKofTVYWhB-be4UGBEfvaGD-8i1xTi5bbw"
+    process.env.NEXT_PUBLIC_SUPABASE_URL_DESIGN!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY_DESIGN!
   );
