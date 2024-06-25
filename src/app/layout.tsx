20971d51df366ce9e2e8230bf15d39ad47add733
Wilson Ibekason <102219342+wilsonibekason@users.noreply.github.com>
2024-06-25 15:51:25 -0700

    working on loader and validations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export async function generateMetadata(): Promise<Metadata> {
     publisher: "Vercel",
     robots: meta.robots,
     icons: { icon: meta.webspirrelogo },
-    // metadataBase: new URL(meta.url),
+    metadataBase: new URL(meta.url),
     openGraph: {
       url: meta.url,
       title: meta.title,
