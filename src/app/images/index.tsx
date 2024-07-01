77e5b57a820b04a9c75c8523641bfdcaf1d98b82
StephenChi-hi <nwikechisom77@gmail.com>
2024-07-01 15:24:12 +0100

    hihh

--- src/app/images/index.tsx
+++ src/app/images/index.tsx
@@ -9,6 +9,29 @@ import img7 from "./img1/img_7.png";
 import img8 from "./img1/img_8.png";
 import img9 from "./img2/img_9.png";
 import img10 from "./img2/img_10.png";
+import img11 from "./img2/img_11.png";
+import img12 from "./img2/img_12.png";
+import img13 from "./img2/img_13.png";
+import img14 from "./img2/img_14.png";
+import img15 from "./img2/img_15.png";
+import img16 from "./img2/img_16.png";
 // Add more imports as needed
 
-export { img1, img2, img3, img4, img5, img6, img7, img8, img9, img10 };
+export {
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+};
